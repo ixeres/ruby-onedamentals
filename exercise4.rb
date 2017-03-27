d620ad15b09211af267e5ce4edc6175ecd2f16c4
Devin Leduc <ixeres@gmail.com>
2017-03-27 14:21:45 -0400

    Fizzed the Buzz.

--- exercise4.rb
+++ exercise4.rb
@@ -1,8 +1,13 @@
 #FIZZBUZZ EXERCISE - BITMAKER EDITION.
 
 (1..100).each do |n|
-  a = String.new
-  puts a << "Bit" if n%3 == 0
-  puts a << "Maker" if n%5 == 0
-  puts a << n.to_s if a.empty?
+  if (n % 3 == 0) && (n % 5 == 0) #boolean to determine if BOTH conditions are met.
+    puts "BitMaker"
+  elsif n % 3 == 0 #otherwise, if first condition is met.
+    puts "Bit"
+  elsif n % 5 == 0 #OTHER otherwise, if second condition is met.
+    puts "Maker"
+  else
+    puts n #other OTHER otherwise. Just put the number.
+  end #But why does it need TWO ends? One for the main operation, and one for the checks?
 end
