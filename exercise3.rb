33de63ccb921226ce4f14017aac356ad8ee6c134
Devin Leduc <ixeres@gmail.com>
2017-03-27 13:41:48 -0400

    Exercise 3, with some Monty Python flair.

---
+++ exercise3.rb
@@ -0,0 +1,15 @@
+puts "What is your name?"
+
+name = gets.chomp
+
+puts "Okay, #{name}. What... Is your quest?"
+
+quest = gets.chomp
+
+puts "Excellent quest, #{quest}, #{name}! Now tell me... What... IS YOUR FAVOURITE COLOUR?"
+
+colour = gets.chomp
+
+puts "...#{colour} is a stupid colour, #{name}."
+
+#Reminder! Variables are assigned like this: variable_name = your variable.
