3722176f4ff18f869325323749311cfd58d40f9c
Devin Leduc <ixeres@gmail.com>
2017-03-27 17:05:27 -0400

    Creating puts for pizza order quantities of zero or one.

--- exercise5.rb
+++ exercise5.rb
@@ -2,3 +2,13 @@ puts "How many pizzas ya want, jabroni?"
 pizzas = gets.chomp.to_i #user input is assigned variable 'pizzas', turned into an integer value with 'to_i'
 if pizzas <= 0
   puts "Then whaddya wasting my time for? Get outta here!" #can't put any toppings on 0 pizzas or less!
+else pizzas == 1
+  puts "A single pizza, huh? How many toppings you want on that bad boy?" #special line for a single pizza.
+  toppings = gets.chomp.to_i
+  if toppings <= 0
+    puts "You've ordered a boring old cheese pizza with no toppings." #1 pizza, no toppings, special clause.
+  else toppings > 0
+    puts "You've ordered a single pizza with #{toppings} toppings." #1 pizza with at least one topping.
+
+  end
+end
