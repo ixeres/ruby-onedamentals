89e2ba76e8e75a21f2dc0253dc82811328796be1
Devin Leduc <ixeres@gmail.com>
2017-03-27 13:16:18 -0400

    Doing some Ruby.

---
+++ exercise1.rb
@@ -0,0 +1,16 @@
+puts 2
+puts 3
+puts 2 + 3
+
+puts 5 / 2
+puts 20 * 5
+puts "Ada Lovelace lived for #{1852 - 1815} years."
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
+puts (1 < 3) && (3 < 5)
+puts (1 > 1) && (2 > 2)
+puts (1 == 2) || (2 < 3)
+puts (1 != 2) || (2 < 3)
+puts (1 == 2) || (2 ==3)
---
+++ exercise2.rb
@@ -0,0 +1,26 @@
+# calculate a good tip for a $55 meal? WELL OKAY.
+
+puts 55 * 0.2
+
+puts "I want this many dogs." + "7"
+
+#Turn integers into strings using quotation marks!
+
+puts "I'm going to multiply #{45628 * 7839}!"
+
+#DON'T FORGET TO PUTS, YOU PUTZ!
+
+puts (10 < 20 && 30 < 20) || !(10==11)
+
+name = "Mr. Butts"
+greeting = "Hello #{name}! Your name is stupid."
+mission = "Your mission, should you choose to accept it... Get a new name."
+
+puts greeting + mission
+
+amount = 20
+new_amount = amount
+new_amount #Should be 20.
+amount = "twenty"
+amount #now it should be "twenty"!
+new_amount #...It WON'T be "twenty", it'll be 20! Because we didn't change the assignment of 'new_amount'!
