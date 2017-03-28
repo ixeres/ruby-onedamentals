a827d9a404c5961c227071d7cf9544f5c58eb832
Devin Leduc <ixeres@gmail.com>
2017-03-27 23:16:04 -0400

    Added energy condition.

--- exercise6.rb
+++ exercise6.rb
@@ -1,6 +1,7 @@
 #starting values
 distance = 0
 home = 0
+energy = 3 #starting energy value
 
 while home == 0
  #creates the loop, ends loop if you return home.
@@ -8,10 +9,14 @@ puts "You are on a quest... For FITNESS. Will you walk or run?"
 speed = gets.chomp
   if speed == "walk"
     distance += 1
-    puts "You've travelled #{distance} kms. You are #{distance} kms from home. Type 'home' to go home."
-  elsif speed == "run"
+    energy += 1
+    puts "You've travelled #{distance} kms. You have #{energy} energy left. Type 'home' to go home."
+  elsif speed == "run" && energy > 0
     distance += 5
-    puts "You've travelled #{distance} kms. You are #{distance} kms from home. Type 'home' to go home."
+    energy -= 1
+    puts "You've travelled #{distance} kms. You have #{energy} energy left. Type 'home' to go home."
+  elsif speed == "run" && energy == 0 #energy condition for running!
+    puts "You are too tired to run! Try walking to regain energy. Or just go home."
   elsif speed == "home"
     home += 1
     if home == 1 #end loop condition
