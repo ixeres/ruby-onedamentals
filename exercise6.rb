eaaeb413dec6cf5249cd8af2199d6222ba292130
Devin Leduc <ixeres@gmail.com>
2017-03-27 23:00:18 -0400

    Created run/walk loop. Added end condition for 'home'.

--- exercise6.rb
+++ exercise6.rb
@@ -1,14 +1,23 @@
 #starting values
 distance = 0
+home = 0
 
+while home == 0
+ #creates the loop, ends loop if you return home.
 puts "You are on a quest... For FITNESS. Will you walk or run?"
 speed = gets.chomp
-if speed == "walk"
-  distance += 1
-  puts "You've walked #{distance} kms."
-elsif speed == "run"
-  distance += 5
-  puts "You've ran #{distance} kms."
-else
-  puts "I can't do that Dave."
+  if speed == "walk"
+    distance += 1
+    puts "You've travelled #{distance} kms. You are #{distance} kms from home. Type 'home' to go home."
+  elsif speed == "run"
+    distance += 5
+    puts "You've travelled #{distance} kms. You are #{distance} kms from home. Type 'home' to go home."
+  elsif speed == "home"
+    home += 1
+    if home == 1 #end loop condition
+    puts "You've returned home! Go get some sleep."
+  end
+  else
+    puts "I can't do that Dave." #for answers that are not 'run' or 'walk'
+end
 end
